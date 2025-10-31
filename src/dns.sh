16c6b1db7cd94ed0d1e88f442247658003bdd8e5
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2025-10-31 19:43:12 -0300

    Update DNS script with Portuguese prompts and messages

--- src/dns.sh
+++ src/dns.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Xray2026 - Configuração de DNS
+# Autor: PhoenixxZ2023
+
 is_dns_list=(
     1.1.1.1
     8.8.8.8
@@ -7,6 +12,7 @@ is_dns_list=(
     set
     none
 )
+
 dns_set() {
     if [[ $1 ]]; then
         case ${1,,} in
@@ -29,21 +35,21 @@ dns_set() {
             if [[ $2 ]]; then
                 is_dns_use=${2,,}
             else
-                ask string is_dns_use "请输入 DNS: "
+                ask string is_dns_use "Digite o DNS desejado: "
             fi
             ;;
         none)
             is_dns_use=none
             ;;
         *)
-            err "无法识别 DNS 参数: $@"
+            err "Parâmetro DNS não reconhecido: $@"
             ;;
         esac
     else
         is_tmp_list=(${is_dns_list[@]})
-        ask list is_dns_use null "\n请选择 DNS:\n"
+        ask list is_dns_use null "\nEscolha o DNS:\n"
         if [[ $is_dns_use == "set" ]]; then
-            ask string is_dns_use "请输入 DNS: "
+            ask string is_dns_use "Digite o DNS desejado: "
         fi
     fi
     if [[ $is_dns_use == "none" ]]; then
@@ -52,5 +58,5 @@ dns_set() {
         cat <<<$(jq '.dns.servers=["'${is_dns_use/https/https+local}'"]' $is_config_json) >$is_config_json
     fi
     manage restart &
-    msg "\n已更新 DNS 为: $(_green $is_dns_use)\n"
+    msg "\nDNS atualizado para: $(_green $is_dns_use)\n"
 }
